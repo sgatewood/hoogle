c419a372f5d0e842595ea37c8128bbcd3cdb9ecb
haotian <haotian@pop-os.localdomain>
2019-02-21 14:54:38 -0500

    haha just committing

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ http.createServer(function(req, res){
     res.writeHead(200, {
         'Content-Type' : 'text/plain' 
     });
-    res.end('hoogle testing page!');
+    res.end('testing mini server for node env');
 }).listen(3000);
 
-console.log('Server running at localhost: 3000 !');
+console.log('Server running at localhost: 3000');
 
